cb642c8d7e25cfc084dcd5551f5f0032120b240e
John <john@beyondvoip.net>
2024-02-14 12:02:28 -0800

    Move create_db, update requirements

--- create_db.py
+++ create_db.py
@@ -12,34 +12,7 @@ DB_NAME = 'initial_db_user_db'
 
 # Establish a connection to the PostgreSQL server
 
-
-
-# Create a cursor object to execute SQL queries
-def check_user_exists():
-    conn = connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_NAME
-    )
-    cursor = conn.cursor()
-    # Check if the user exists
-    cursor.execute(sql.SQL("SELECT 1 FROM pg_roles WHERE rolname='cryptmaster'"))
-    existing_user_check = cursor.fetchone()
-    if not existing_user_check:
-        print(f"Creating user: 'cryptmaster'")
-        cursor.execute(sql.SQL(f"CREATE USER cryptmaster PASSWORD '{get_db_secret()}'"))
-    else:
-        print(f"User 'cryptmaster' already exists.")
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return
-
-
-def check_db_exists():
-    conn = connect(
+conn = connect(
         host=DB_HOST,
         port=DB_PORT,
         user=DB_USER,
@@ -47,44 +20,32 @@ def check_db_exists():
         database=DB_NAME,
         autocommit=True
     )
-    with conn.cursor() as cursor:
-         # Check if the database exists
-        cursor.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname='cryptmaster_db'"))
-        existing_db_check = cursor.fetchone()
-        if not existing_db_check:
-            cursor.autocommit=True
-            print(f"Creating database: 'cryptmaster_db'")
-            cursor.execute(sql.SQL("CREATE DATABASE cryptmaster_db WITH OWNER = cryptmaster"))
-        else:
-            print(f"Database 'cryptmaster_db' already exists.")
-        #conn.commit()
-        cursor.close()
-        conn.close()
-    return True
 
-def db_cleanup():
-    conn = connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_NAME
-    )
-    cursor = conn.cursor()
-    cursor.execute(sql.SQL("DROP DATABASE IF EXISTS initial_db_user_db"))
-    conn.commit()
-    cursor.execute(sql.SQL("DROP USER IF EXISTS initial_db_user"))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return
+# Create a cursor object to execute SQL queries
+cursor = conn.cursor()
+
+# Check if the user exists
+cursor.execute(sql.SQL("SELECT 1 FROM pg_roles WHERE rolname='cryptmaster'"))
+existing_user_check = cursor.fetchone()
+
+if not existing_user_check:
+    print(f"Creating user: 'cryptmaster'")
+    cursor.execute(sql.SQL(f"CREATE USER cryptmaster PASSWORD '{get_db_secret()}'"))
+else:
+    print(f"User 'cryptmaster' already exists.")
+
+cursor.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname='cryptmaster_db'"))
+existing_db_check = cursor.fetchone()
+if not existing_db_check:
+    print(f"Creating database: 'cryptmaster_db'")
+    cursor.execute(sql.SQL("CREATE DATABASE cryptmaster_db WITH OWNER = cryptmaster"))
+else:
+    print(f"Database 'cryptmaster_db' already exists.")
+
+cursor.execute(sql.SQL("DROP DATABASE IF EXISTS initial_db_user_db"))
+cursor.execute(sql.SQL("DROP USER IF EXISTS initial_db_user"))
 
 
-def db_check():
-    check_user_exists()
-    check_db_exists()
-    db_cleanup()
-    return
-
-
-db_check()
+conn.commit()
+cursor.close()
+conn.close()
