a260e8f4418f58761b1fcea25e678d5b14b27274
John <john@beyondvoip.net>
2024-02-15 09:05:03 -0800

    Finish db connectivity file

---
+++ utilities/send_alerts.py
@@ -0,0 +1,9 @@
+# This script is a placeholder.  If you would like to have your cryptmaster send alerts,
+# turn on send alerts.  You will need to customize this script to provide the logic for sending alerts.
+# Bear in mind that all network traffic should be very tightly controlled.
+
+
+
+def send_alerts(alert_type, alert_message):
+    print(f'Alert Type - {alert_type} | Alert Message - {alert_message}')
+    # add your logic here to send an alert to sms, email, etc.
