6651146d60cca35a1c6ba669f90a83704ad3dd53
John <john@beyondvoip.net>
2024-02-15 13:29:21 -0800

    Update Readme

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 # Lots of work left for this script
 
 from utilities.database_connectivity import query_db, execute_db
+
+
+# New users will require both a password, and a one time passcode
+# Password will be used to encrypt the OTP seed info
+# We will first try to decrypt the users seed with their password, if that fails, then user has sent bad password
+# If successful, we decrypt the OTP seed, and verify the user has sent a good OTP.
