a7b1df26b39905382d8a8e497bb1d322e456522b
John <john@beyondvoip.net>
2024-02-15 09:17:10 -0800

    Finish db connectivity file

---
+++ utilities/new_db_entries.sql
@@ -0,0 +1,3 @@
+INSERT INTO system_info(version_active) VALUES('2.0.0')
+INSERT INTO cryptmaster_warden(system_online) VALUES(True)
+INSERT INTO event_log(event_type, details) VALUES('System Install', 'Installed CryptMasterServer version 2.0.0')
