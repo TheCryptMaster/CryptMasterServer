b090326fb795ecdfbe62194036383c691838ec58
John <john@beyondvoip.net>
2024-04-15 16:14:43 -0700

    Update Open API

--- utilities/system_data.py
+++ utilities/system_data.py
@@ -1,12 +1,31 @@
-import uuid
+import os
+import sys
+
 
 def get_system_id():
-    id = uuid.uuid1()
-    return id.hex[-24:]
+    ### print('Currently only working with ubuntu')
+    """ Find OS and run appropriate read mobo serial num command"""
+    os_type = sys.platform.lower()
+    if "darwin" in os_type:
+        command = "ioreg -l | grep IOPlatformSerialNumber"
+    elif "win" in os_type:
+        command = "wmic bios get serialnumber"
+    elif "linux" in os_type:
+        command = "ls -l /dev/disk/by-uuid"
+    response = response = os.popen(command).read()
+    return locate_serial(response)
 
-def clean_result(string_info):
-    new_string = ''
-    for letter in string_info:
-        if letter not in ['_', '.']:
-            new_string += letter
-    return new_string
+
+
+def locate_serial(response):
+    serial, serial_text = '', None
+    words = response.split()
+    for word in words:
+        if '-' in word:
+            serial_text = word
+            break
+    if serial_text:
+        for letter in word:
+            if letter != '-':
+                serial += letter
+    return serial
